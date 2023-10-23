43b50a0034a4a0636289b8f93b1ac3420b700394
tomas-sedlak <t.sedlak.24@gmail.com>
2023-10-23 16:03:24 +0200

    Pridane upratovanie

--- digital_number.py
+++ digital_number.py
@@ -1,44 +1,77 @@
-import typhoon
-
-robot = typhoon.Typhoon("COM13")
-
-number = int(input("Cislo v desiatkovej sustave: "))
-digit = bin(number).replace("0b", "")
-print(number, "v dvojkovej sustave je", digit)
-digit = digit[::-1]
+import typhoon, time
 
 poz_d_x, poz_d_y = 100, 98
 zobrat_0_x, zobrat_0_y = 20, 95
 zobrat_1_x, zobrat_1_y = 60, 95
 zdvih = 50
 
-robot.send_x_y_z_pw(0, 0, zdvih, 0)  # zaciatocna pozicia
+robot = typhoon.Typhoon("COM13") # toto zmen ak ti program nefunguje
 
-for d in digit:
-    if d == "0":
-        robot.send_x_y_z_pw(zobrat_0_x, zobrat_0_y, zdvih, 0)
-        robot.send_x_y_z_pw(zobrat_0_x, zobrat_0_y, 0, 255)
-        robot.send_x_y_z_pw(zobrat_0_x, zobrat_0_y, zdvih, 255)
+def move(robot, binary_num):
+    binary_num = binary_num[::-1]
+    start_x, start_y = poz_d_x, poz_d_y
+    zero_x, zero_y = zobrat_0_x, zobrat_0_y
+    one_x, one_y = zobrat_1_x, zobrat_1_y
 
-        print(d, 'z pozicie x:', zobrat_0_x, 'y:', zobrat_0_y)
-        zobrat_0_y -= 30
+    robot.send_x_y_z_pw(0, 0, zdvih, 0)  # zaciatocna pozicia
 
-    elif d == "1":
-        robot.send_x_y_z_pw(zobrat_1_x, zobrat_1_y, zdvih, 0)
-        robot.send_x_y_z_pw(zobrat_1_x, zobrat_1_y, 0, 255)
-        robot.send_x_y_z_pw(zobrat_1_x, zobrat_1_y, zdvih, 255)
+    for digit in binary_num:
+        if digit == "0":
+            grab(zero_x, zero_y)
+            zero_y -= 30
+        elif digit == "1":
+            grab(one_x, one_y)
+            one_y -= 30
+        
+        place(start_x, start_y)
+        start_y -= 30
 
-        print(d, 'z pozicie x:', zobrat_1_x, 'y:', zobrat_1_y)
-        zobrat_1_y -= 30
+    # konecna pozicia
+    robot.send_x_y_z_pw(0, 0, zdvih, 0)
+    robot.send_x_y_z_pw(0, 0, 0, 0)
+
+def clean(robot, binary_num):
+    start_x, start_y = poz_d_x, poz_d_y
+    zero_x, zero_y = zobrat_0_x, zobrat_0_y
+    one_x, one_y = zobrat_1_x, zobrat_1_y
+
+    robot.send_x_y_z_pw(0, 0, zdvih, 0)  # zaciatocna pozicia
+
+    for digit in binary_num:
+        grab(start_x, start_y)
+        start_y -= 30
+
+        if digit == "0":
+            place(zero_x, zero_y)
+            zero_y -= 30
+        elif digit == "1":
+            place(one_x, one_y)
+            one_y -= 30
+
+    # konecna pozicia
+    robot.send_x_y_z_pw(0, 0, zdvih, 0)
+    robot.send_x_y_z_pw(0, 0, 0, 0)
+
+def grab(x, y):
+    robot.send_x_y_z_pw(x, y, zdvih, 0)
+    robot.send_x_y_z_pw(x, y, 0, 0)
+    robot.send_x_y_z_pw(x, y, 0, 255)
+    robot.send_x_y_z_pw(x, y, zdvih, 255)
+
+def place(x, y):
+    robot.send_x_y_z_pw(x, y, zdvih, 255)
+    robot.send_x_y_z_pw(x, y, 0, 255)
+    robot.send_x_y_z_pw(x, y, 0, 0)
+    robot.send_x_y_z_pw(x, y, zdvih, 0)
+
+while True:
+    decimal_num = int(input("Cislo v desiatkovej sustave: "))
+    binary_num = bin(decimal_num)[2:]
     
-    robot.send_x_y_z_pw(poz_d_x, poz_d_y, zdvih, 255)
-    robot.send_x_y_z_pw(poz_d_x, poz_d_y, 0, 255)
-    robot.send_x_y_z_pw(poz_d_x, poz_d_y, 0, 0)
-    robot.send_x_y_z_pw(poz_d_x, poz_d_y, zdvih, 0)
+    print(binary_num, "v dvojkovej sustave je", decimal_num)
+    move(robot, binary_num)
 
-    print(' umiestnit na x:', poz_d_x, 'y:', poz_d_y)
-    poz_d_y -= 30
+    time.sleep(2)
 
-# konecna pozicia
-robot.send_x_y_z_pw(0, 0, zdvih, 0)
-robot.send_x_y_z_pw(0, 0, 0, 0)
+    print("Upratujem...")
+    clean(robot, binary_num)
