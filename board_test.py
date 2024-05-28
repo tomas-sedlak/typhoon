7d074a65c4c856278cb96da80fc22479d71570ee
Cafe <cafe@srobarka.sk>
2024-05-28 09:28:00 +0200

    Added board test

---
+++ board_test.py
@@ -0,0 +1,57 @@
+# Fonkcne, musi byt time.sleep(0.001)
+import time, tkinter
+import RPi.GPIO as GPIO
+
+size, rect = 400, 400/8
+canvas = tkinter.Canvas(width=size, height=size)
+canvas.pack()
+
+pole_st = [[1] * 9] * 9 # Vytvorenie prazdej sachovnice
+pole = [[1] * 9] * 9 # Vytvorenie prazdej sachovnice
+
+def draw():
+    for row in range(8):
+        for col in range(8):
+            color = "yellow" if pole[row][col] else "red"
+            canvas.create_rectangle(rect * col, rect * row, rect * (col + 1), rect * (row + 1), fill=color)
+
+def tah():
+    global pole
+    r, s = [0] * 10, [0] * 10
+    s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8] = 17, 27, 22, 5, 6, 13, 19, 26
+    r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8] = 2, 3, 4, 25, 8, 7, 23, 24
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    for i in range(1, 9): GPIO.setup(s[i], GPIO.IN)
+    for i in range(1, 9): GPIO.setup(r[i], GPIO.OUT)
+    # Ak je na sachovnici figurka pole[cr][cs]=1 a naopak. Vyhodnotenie pomocou fototranzistora.
+    # Ak nie je nad fototranzistorom figurka, fototranzistor vedie prud: - if GPIO.input(s[cs])==1:pole[cr][cs]=0                                                                              # - else:pole[cr][cs]=1
+
+    for cr in range(1, 9):
+        GPIO.output(r[cr], 1)
+        for cs in range(1, 9):
+            GPIO.input(s[cs])
+            if GPIO.input(s[cs]) == 1:
+                pole[cr][cs] = 0
+            if GPIO.input(s[cs]) == 0:
+                pole[cr][cs] = 1
+            # print(GPIO.input(s[cs]),end='')
+            time.sleep(0.001)
+            # print()
+        GPIO.output(r[cr], 0)
+        time.sleep(0.001)
+    GPIO.cleanup()
+    return pole
+
+
+
+while True:
+    pole_st = tah()
+    print('*********************')
+    for r in range(1, 9):
+        for s in range(1, 9):
+            print(pole_st[r][s], end='')
+
+        print()
+    # time.sleep(0.5)
